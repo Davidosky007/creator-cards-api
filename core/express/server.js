be74a287ec26991baafd3c1fd4ab88940930d933
David Bassey <davidosky1@gmail.com>
2026-06-17 01:19:55 +0100

    feat: add creator-card messages

--- core/express/server.js
+++ core/express/server.js
@@ -248,7 +248,7 @@ function Server(serverConfig = {}) {
           : 'Some error occured.';
         responseComponents.body.code = error.isApplicationError ? error.errorCode : undefined;
         responseComponents.body.errors = error.details || undefined;
-        responseComponents.body.data = error.context;
+      responseComponents.body.data = error.context;
 
         expressResponse.status(responseComponents.statusCode).json(responseComponents.body); // Todo: Add a callback config that can be used to handle this in a custom way.
       } finally {
